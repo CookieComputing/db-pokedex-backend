d95db275e71c4a47f871ead684ae32bf70faec53
Kevin Hui <hui.k@northeastern.edu>
2021-11-24 19:05:15 -0500

    set on delete constraint to set null

--- pokedex/pokemon/models.py
+++ pokedex/pokemon/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class PokemonInfo(models.Model):
     national_num = models.IntegerField(primary_key=True)
-    evolved_state_pkid = models.ForeignKey('self', models.DO_NOTHING, db_column='evolved_state_pkid', blank=True, null=True, related_name="evolved_state")
-    devolved_state_pkid = models.ForeignKey('self', models.DO_NOTHING, db_column='devolved_state_pkid', blank=True, null=True, related_name="devolved_state")
+    evolved_state_pkid = models.ForeignKey('self', models.SET_NULL, db_column='evolved_state_pkid', blank=True, null=True, related_name="evolved_state")
+    devolved_state_pkid = models.ForeignKey('self', models.SET_NULL, db_column='devolved_state_pkid', blank=True, null=True, related_name="devolved_state")
     name = models.CharField(max_length=45)
     photo_url = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
